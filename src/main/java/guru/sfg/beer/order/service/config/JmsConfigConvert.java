9c6759f4b93b39fc6ed9a0ce1c6e2fd23e81dafb
SubhankarMukherjee <connecttosubhankar@gmail.com>
2021-11-25 02:33:46 +0530

    done with IT

---
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfigConvert.java
@@ -0,0 +1,34 @@
+package guru.sfg.beer.order.service.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+
+@Configuration
+
+public class JmsConfigConvert {
+
+    public static final String BFREWING_REQUEST_QUEUE= "brewing-request";
+    public static final String INVENTORY_EVENT_QUEUE= "inventory_queue";
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
+    public static final String VALIDATE_ORDER_RESPONSE_QUEUE="validate-result";
+    public static final String ALLOCATE_ORDER_QUEUE="allocate-order";
+    public static final String ALLOCATE_ORDER_RESPONSE_QUEUE="allocate-result";
+
+    //public static final String MY_SEND_RCV_QUEUE="send-receive";
+    @Bean
+    public MessageConverter messageConverter(ObjectMapper objectMapper)
+    {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        converter.setObjectMapper(objectMapper);  // set spring boot managed instance of object mapper
+        return converter;
+    }
+
+
+}
---
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -0,0 +1,28 @@
+package guru.sfg.beer.order.service.web.mappers;
+
+import com.comon.brewery.model.BeerOrderLineDto;
+import guru.sfg.beer.order.service.domain.BeerOrderLine;
+import javax.annotation.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-11-25T02:31:39+0530",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_291 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerOrderLineMapperImpl extends BeerOrderLineMapperDecorator implements BeerOrderLineMapper {
+
+    @Autowired
+    @Qualifier("delegate")
+    private BeerOrderLineMapper delegate;
+
+    @Override
+    public BeerOrderLine dtoToBeerOrderLine(BeerOrderLineDto dto)  {
+        return delegate.dtoToBeerOrderLine( dto );
+    }
+}
